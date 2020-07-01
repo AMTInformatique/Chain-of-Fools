39063b1455beca337cb12168c09a51f6d29de6ad
Sam-prog-sudo <61703841+Sam-prog-sudo@users.noreply.github.com>
2020-07-01 19:41:26 +0200

    ajout de new_block

--- Block.py
+++ Block.py
@@ -1,27 +1,32 @@
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []    
         # créer le premier block
-        self.new_block(previous_hash=1, proof=100)
+        self.new_block(proof=100, previous_hash=1)
         
-    def new_block(self):
-        # créer un nouveau block et l'ajoute à la chaine
-        pass
-    
-    def new_transaction(self):
-        # ajoute une nouvelle transaction à la liste des transactions
-        pass
-    
-    @staticmethod
-    def hash(block):
-        # hashe un block
-        pass
+    def new_block(self, proof, previous_hash=None,):
+        """
+        Créer un nouveau block dans la blockchain
+        :param proof: <int> La preuve donnée par l'algorithme de preuve de travail
+        :param previous_hash: (Optionel) <str> Hash du block précédent 
+        :return: <dict> nouveau block
+        """
 
-    @property
-    def last_block(self):
-        # retourn le dernier block de la chaine
-        pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # RàZ de la list courante des transactions
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
     
     def new_transaction(self, sender, recipient, amount):
         """
@@ -39,3 +44,15 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+    
+    @staticmethod
+    def hash(block):
+        # hashe un block
+        pass
+
+    @property
+    def last_block(self):
+        # retourn le dernier block de la chaine
+        pass
+    
+
